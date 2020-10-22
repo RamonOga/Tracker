026a4d5408f188d237c2121789542e0a283ce802
Markelov Roman <roman.sercent@gmail.com>
2020-10-22 12:33:26 +0300

    1. Паспорт и жители [#369596]

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -1,6 +1,5 @@
 package ru.job4j.collection;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,21 +7,15 @@ public class PassportOffice {
     private Map<String, Citizen> citizens = new HashMap<>();
 
     public boolean add(Citizen citizen) {
-        boolean rsl = true;
+        boolean rsl = false;
             if (!citizens.containsValue(citizen)) {
                 citizens.put(citizen.getPassport(), citizen);
-                rsl = false;
+                rsl = true;
         }
         return rsl;
     }
 
     public Citizen get(String passport) {
-        Citizen rsl = null;
-        for (String key : citizens.keySet()) {
-            if (citizens.get(key).getPassport() == passport) {
-                rsl = citizens.get(key);
-            }
-        }
-        return rsl;
+        return citizens.get(passport);
     }
 }
