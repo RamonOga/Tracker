0e607ec9867cbbf1c7d7cabbfb3dff1b4ec15313
Markelov Roman <roman.sercent@gmail.com>
2020-10-22 23:36:33 +0300

    0. Сортировка [#369585]

---
+++ src/main/java/ru/job4j/tracker/SortByIdItemReverce.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import java.util.Comparator;
+
+public class SortByIdItemReverce implements Comparator<Item> {
+    @Override
+    public int compare(Item o1, Item o2) {
+        return Integer.compare(o2.getId(), o1.getId());
+    }
+
+
+}
