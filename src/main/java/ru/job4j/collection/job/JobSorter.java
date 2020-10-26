e691160451133d0da09a69bdd4602678532a1db7
Markelov Roman <roman.sercent@gmail.com>
2020-10-26 16:44:30 +0300

    2. Комбинированный компаратор [#369584]

--- src/main/java/ru/job4j/collection/job/JobSorter.java
+++ src/main/java/ru/job4j/collection/job/JobSorter.java
@@ -2,19 +2,17 @@ package ru.job4j.collection.job;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class JobSorter {
     public static void main(String[] args) {
+
         List<Job> jobs = Arrays.asList(
-                new Job("Fix bugs", 4),
-                new Job("Zmpl task", 2),
-                new Job("Reboot server", 1)
+                new Job("Sigma", 1),
+                new Job("Alpha", 4),
+                new Job("Gamma", 2),
+                new Job("Beta", 0)
         );
-        System.out.println(jobs);
-        Collections.sort(jobs);
-        System.out.println(jobs);
-        Collections.sort(jobs, new SortByNameJob());
-        System.out.println(jobs);
     }
 }
