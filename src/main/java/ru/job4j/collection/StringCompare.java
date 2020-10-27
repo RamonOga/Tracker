05a02e98bcf8c5726b84ee1d7e0844d401b4be5a
Markelov Roman <roman.sercent@gmail.com>
2020-10-27 14:59:23 +0300

    StringCompare review

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,17 +5,15 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int rsl;
         char[] leftArr = left.toCharArray();
         char[] rightArr = right.toCharArray();
         int minSize = Math.min(left.length(), right.length());
-        rsl = Integer.compare(left.length(), right.length());
         for (int i = 0; i < minSize; i++) {
-            int temp = Character.compare(leftArr[i], rightArr[i]);
-            if (temp != 0) {
-                return temp;
+            int rsl = Character.compare(leftArr[i], rightArr[i]);
+            if (rsl != 0) {
+                return rsl;
             }
         }
-        return rsl;
+        return Integer.compare(left.length(), right.length());
     }
 }
