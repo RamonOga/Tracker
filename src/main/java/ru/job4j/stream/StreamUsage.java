559f83511517499ddc49e517c447db87ad6dd120
Markelov Roman <roman.sercent@gmail.com>
2020-10-31 19:10:30 +0300

    0. Stream API [#369421] ver3

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -1,5 +1,4 @@
 package ru.job4j.stream;
-import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,11 +6,17 @@ import java.util.stream.Collectors;
 public class StreamUsage {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>
-                            (List.of(-1, 1, -2, 2, -3, 3, -4, 4, -5, 5, -6, 6))
-                            .stream().filter(x -> x > 0)
-                            .collect(Collectors.toList());
+        List<Integer> list = StreamUsage.positiveFilter(
+                new ArrayList<>(List.of(
+                        -1, 1, -2, 2, -3, 3, -4, 4, -5, 5, -6, 6
+                )));
+    }
 
+    public static List<Integer> positiveFilter(List<Integer> input) {
+        return input
+               .stream()
+               .filter(x -> x > 0)
+               .collect(Collectors.toList());
     }
 
 
