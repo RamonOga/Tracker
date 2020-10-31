1aa2e30a861518eb085f24ef5397233a33e080de
Markelov Roman <roman.sercent@gmail.com>
2020-10-31 19:01:49 +0300

    0. Stream API [#369421] ver1

---
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,19 @@
+package ru.job4j.stream;
+import javax.sound.midi.Soundbank;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(List.of(-1, 1, -2, 2, -3, 3, -4, 4, -5, 5, -6, 6))
+        list = list.stream().filter(x -> x > 0)
+               .collect(Collectors.toList());
+
+    }
+
+
+
+
+}
