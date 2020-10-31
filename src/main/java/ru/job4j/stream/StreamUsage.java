0fc06eff19c4a57e5aadc3d029f428854fff5349
Markelov Roman <roman.sercent@gmail.com>
2020-10-31 19:02:55 +0300

    0. Stream API [#369421] ver2

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -7,9 +7,10 @@ import java.util.stream.Collectors;
 public class StreamUsage {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(List.of(-1, 1, -2, 2, -3, 3, -4, 4, -5, 5, -6, 6))
-        list = list.stream().filter(x -> x > 0)
-               .collect(Collectors.toList());
+        List<Integer> list = new ArrayList<>
+                            (List.of(-1, 1, -2, 2, -3, 3, -4, 4, -5, 5, -6, 6))
+                            .stream().filter(x -> x > 0)
+                            .collect(Collectors.toList());
 
     }
 
