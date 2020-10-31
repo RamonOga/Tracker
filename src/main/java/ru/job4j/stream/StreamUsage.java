2d815abf314f58059df58438c97fffe9cb7372da
Markelov Roman <roman.sercent@gmail.com>
2020-10-31 19:28:59 +0300

    0. Stream API [#369421] ver1 refactoring.

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -6,20 +6,11 @@ import java.util.stream.Collectors;
 public class StreamUsage {
 
     public static void main(String[] args) {
-        List<Integer> list = StreamUsage.positiveFilter(
-                new ArrayList<>(List.of(
-                        -1, 1, -2, 2, -3, 3, -4, 4, -5, 5, -6, 6
-                )));
+        List<Integer> list = new ArrayList<>
+                (List.of(-1, 1, -2, 2, -3, 3, -4, 4, -5, 5, -6, 6));
+        list = list.stream()
+                .filter(x -> x > 0)
+                .collect(Collectors.toList());
+
     }
-
-    public static List<Integer> positiveFilter(List<Integer> input) {
-        return input
-               .stream()
-               .filter(x -> x > 0)
-               .collect(Collectors.toList());
-    }
-
-
-
-
 }
