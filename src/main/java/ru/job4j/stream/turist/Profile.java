9dfeec45332966e5a2e381b05210a6b279211f07
Markelov Roman <roman.sercent@gmail.com>
2020-11-02 21:40:31 +0300

    3. Уникальность элементов и сортировка. [#369424]

--- src/main/java/ru/job4j/stream/turist/Profile.java
+++ src/main/java/ru/job4j/stream/turist/Profile.java
@@ -1,7 +1,9 @@
 package ru.job4j.stream.turist;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.PropertyPermission;
 import java.util.stream.Collectors;
 
 public class Profile {
@@ -16,10 +18,28 @@ public class Profile {
     }
 
     static List<Address> collect(List<Profile> input) {
-        return input.stream().
-                sorted(Comparator.comparing(o -> o.getAddress().getCity()))
-                .distinct()
-                .map( s -> s.getAddress())
+        return input.stream()
+                        .sorted(Comparator.comparing( o -> o.getAddress().getCity()))
+                        .map(Profile::getAddress)
+                        .distinct()
+                        .collect(Collectors.toList());
+
+    }
+
+    public static void main(String[] args) {
+        List<Profile> proList = new ArrayList<>(List.of(
+                new Profile(new Address("Msk", "Lenin st", 1, 1))
+                , new Profile(new Address("Spb", "Marks st", 2, 2))
+                , new Profile(new Address("Kzn", "Stalin st", 3, 4))
+                , new Profile(new Address("Elb", "Telman st", 2, 2))
+                , new Profile(new Address("Elb", "Telman st", 2, 2))
+        ));
+        List<Address> a = proList.stream()
+                .map(p -> p.getAddress())
                 .collect(Collectors.toList());
+
+        System.out.println(a);
+
+                System.out.println(Profile.collect(proList));
     }
 }
--- src/test/java/ru/job4j/stream/turist/ProfileTest.java
+++ src/test/java/ru/job4j/stream/turist/ProfileTest.java
@@ -8,23 +8,24 @@ import java.util.List;
 public class ProfileTest {
 
     @Test
-    public void test() {
+    public void collectTest() {
         List<Profile> proList = new ArrayList<>(List.of(
                 new Profile(new Address("Msk", "Lenin st", 1, 1))
                 , new Profile(new Address("Spb", "Marks st", 2, 2))
                 , new Profile(new Address("Kzn", "Stalin st", 3, 4))
                 , new Profile(new Address("Elb", "Telman st", 2, 2))
+                , new Profile(new Address("Elb", "Telman st", 2, 2))
         ));
-        List<Address> expect = List.of(new Address("Msk", "Lenin st", 1, 1)
-                , new Address("Spb", "Marks st", 2, 2)
+        List<Address> expect = List.of(
+                new Address("Elb", "Telman st", 2, 2)
                 , new Address("Kzn", "Stalin st", 3, 4)
-                , new Address("Elb", "Telman st", 2, 2)
+                , new Address("Msk", "Lenin st", 1, 1)
+                , new Address("Spb", "Marks st", 2, 2)
+
+
         );
         List<Address> rsl = Profile.collect(proList);
         Assert.assertEquals(rsl, expect);
 
     }
-
-
-
 }
