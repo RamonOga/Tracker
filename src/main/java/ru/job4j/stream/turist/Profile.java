e818fb44ff4d6da31630b6225f73d0680b856b7e
Markelov Roman <roman.sercent@gmail.com>
2020-11-02 20:19:48 +0300

    2. Список адресов [#369423]

---
+++ src/main/java/ru/job4j/stream/turist/Profile.java
@@ -0,0 +1,22 @@
+package ru.job4j.stream.turist;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profile {
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public Profile(Address address) {
+        this.address = address;
+    }
+
+    static List<Address> collect(List<Profile> input) {
+        return input.stream().
+                map( s -> s.getAddress()).
+                collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/ru/job4j/stream/turist/ProfileTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.stream.turist;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProfileTest {
+
+    @Test
+    public void test() {
+        List<Profile> proList = new ArrayList<>(List.of(
+                new Profile(new Address("Msk", "Lenin st", 1, 1))
+                , new Profile(new Address("Spb", "Marks st", 2, 2))
+                , new Profile(new Address("Kzn", "Stalin st", 3, 4))
+                , new Profile(new Address("Elb", "Telman st", 2, 2))
+        ));
+        List<Address> expect = List.of(new Address("Msk", "Lenin st", 1, 1)
+                , new Address("Spb", "Marks st", 2, 2)
+                , new Address("Kzn", "Stalin st", 3, 4)
+                , new Address("Elb", "Telman st", 2, 2)
+        );
+        List<Address> rsl = Profile.collect(proList);
+        Assert.assertEquals(rsl, expect);
+
+    }
+
+
+
+}
