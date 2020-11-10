1ceaba1215afdf9362800635678db429e8952f42
Markelov Roman <roman.sercent@gmail.com>
2020-11-10 13:28:04 +0300

    2. Аттестация [#369439]

--- src/main/java/ru/job4j/stream/school/Analyze.java
+++ src/main/java/ru/job4j/stream/school/Analyze.java
@@ -3,6 +3,7 @@ package ru.job4j.stream.school;
 import javax.swing.*;
 import java.security.KeyStore;
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collector;
@@ -36,7 +37,7 @@ public class Analyze {
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
         return stream.flatMap(pup -> pup.getSubjects().stream())
                 .collect(Collectors
-                        .groupingBy(Subject::getName,
+                        .groupingBy(Subject::getName, LinkedHashMap::new,
                         Collectors.averagingDouble(Subject::getScore)))
                       .entrySet().stream()
               .map((a) -> new Tuple(a.getKey(), a.getValue()))
