fc02eaed89f43337bdb1334a587712c73f35ffbc
Markelov Roman <roman.sercent@gmail.com>
2020-11-13 11:13:04 +0300

    shit codding.

---
+++ src/main/java/ru/job4j/stream/easystream/EasyStream.java
@@ -0,0 +1,72 @@
+package ru.job4j.stream.easystream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EasyStream {
+
+    static List<Integer> list;
+
+    private EasyStream() {
+    }
+
+    private EasyStream(List<Integer> input) {
+        list = input;
+    }
+
+    private void setList(List<Integer> list) {
+        EasyStream.list = list;
+    }
+
+    private static List<Integer> getList() {
+        return list;
+    }
+
+    private EasyStream getStream() {
+        return this;
+    }
+
+    public static EasyStream of(List<Integer> source) {
+    //throw new UnsupportedOperationException();
+       // EasyStream rsl = new EasyStream();
+      //  rsl.setList(source);
+        return new EasyStream(source);
+    }
+
+        public EasyStream map(Function<Integer, Integer> fun) {
+        List<Integer> rslList = new ArrayList<>();
+            for(Integer i : list) {
+               rslList.add(fun.apply(i));
+            }
+            list = rslList;
+            //throw new UnsupportedOperationException();
+            return getStream();
+        }
+
+        public EasyStream filter(Predicate<Integer> fun) {
+        List<Integer> rslList = new ArrayList<>();
+            for (Integer i : list) {
+                if(fun.test(i)){
+                    rslList.add(i);
+                }
+            }
+            list = rslList;
+           // throw new UnsupportedOperationException();
+            return getStream();
+        }
+
+        public List<Integer> collect() {
+            //throw new UnsupportedOperationException();
+            return list;
+        }
+
+    public static void main(String[] args) {
+        EasyStream.of(List.of(1, 2, 3))
+                .map(e -> e * 2)
+                .filter(e -> e == 2)
+                .collect().forEach(System.out::println);
+    }
+
+}
