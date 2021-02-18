2b81a717ab55d10a2de57eb3541b8c2536d7e913
Roman Markelov <Roman.sercent@gmail.com>
2021-02-18 15:19:00 +0300

    1. Sql схема для проекта Tracker. [#407665]

---
+++ src/main/java/ru/job4j/tracker/db/create.sql
@@ -0,0 +1,4 @@
+create table items_db (
+    id serial primary key,
+    name text
+);
---
+++ src/main/java/ru/job4j/tracker/db/insert.sql
@@ -0,0 +1,4 @@
+insert into items_db (name) values ('item1');
+insert into items_db (name) values ('item2');
+insert into items_db (name) values ('item3');
+insert into items_db (name) values ('item4');
