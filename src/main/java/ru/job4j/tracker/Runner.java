b2b44be11c21ef1e3401919170970e9f0841193d
Roman Markelov <Roman.sercent@gmail.com>
2021-08-12 18:50:38 +0300

    runner and item

--- src/main/java/ru/job4j/tracker/Runner.java
+++ src/main/java/ru/job4j/tracker/Runner.java
@@ -5,18 +5,56 @@ import ru.job4j.tracker.store.HbmTracker;
 import ru.job4j.tracker.store.SqlTracker;
 import ru.job4j.tracker.store.Store;
 
+import java.io.BufferedReader;
 import java.sql.Timestamp;
 
 public class Runner {
     public static void main(String[] args) {
-        Store tracker = new HbmTracker();
-        Item item1 = new Item(0, "name1", "description1");
-        Item item2 = new Item(0, "name1", "description1");
-        Item itemforUpdate = new Item(0, "UpdateName", "UpdateDescription");
 
-
-        System.out.println(tracker.findByName("FindByNameTest"));
-
+        findById();
 
     }
+
+    public static void add() {
+        HbmTracker tracker = new HbmTracker();
+        tracker.add(new Item(0, "name10", "desc1"));
+    }
+
+    /**
+     * .IllegalStateException
+     */
+    public static void replace() {
+        HbmTracker tracker = new HbmTracker();
+        System.out.println(tracker.replace("6",  new Item(0, "Replaced Name1", "Replaced Desc1")) + "<======================================================");
+        System.out.println(tracker.replace("66",  new Item(0, "Replaced Name2", "Replaced Desc2")) + "<======================================================");
+    }
+
+    public static void delete() {
+        HbmTracker tracker = new HbmTracker();
+        int id = tracker.add(new Item(0, "Deleted Name1", "Deleted Desc1")).getId();
+        tracker.delete(String.valueOf(id));
+        tracker.delete("55");
+    }
+
+    public static void findAll() {
+        HbmTracker tracker = new HbmTracker();
+        System.out.println(tracker.findAll());
+    }
+
+    public static void findByName() {
+        HbmTracker tracker = new HbmTracker();
+        tracker.add(new Item(0, "FindByName1", "desc1"));
+        tracker.add(new Item(0, "FindByName1", "desc2"));
+        System.out.println(tracker.findByName("FindByName1"));
+        System.out.println(tracker.findByName("ebobo"));
+    }
+
+    public static void findById() {
+        HbmTracker tracker = new HbmTracker();
+        int id = tracker.add(new Item(0, "FindById1", "descFID1")).getId();
+        System.out.println(tracker.findById(String.valueOf(id)));
+        tracker.findById("123312");
+
+    }
+
 }
--- src/main/java/ru/job4j/tracker/model/Item.java
+++ src/main/java/ru/job4j/tracker/model/Item.java
@@ -77,6 +77,7 @@ public class Item {
                 + "id=" + id
                 + ", name='" + name + '\''
                 + ", created=" + created
+                + ", description=" + description
                 + '}';
 
     }
